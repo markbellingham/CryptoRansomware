ffcaa39ee89e360a072ceea6536480a67a7c7b97
Mark Bellingham <14032098@stu.mmu.ac.uk>
2016-11-16 14:08:17 +0000

    filename changed to variable

--- product_cipher.py
+++ product_cipher.py
@@ -10,11 +10,12 @@ from base import switchMode
 # Function that runs the core program
 def main():
 	# Read text from file
-	with open('test.txt', 'r') as file:
+	working_file = 'confirm_order.php'
+	with open(working_file, 'r') as file:
 		msg = file.read()
 
 	# Get the key and mode from the user
-	print ('msg is: %s' % msg)
+	#print ('msg is: %s' % msg)
 	key_1 = input('Input the key: ')
 	mode_1 = input('Select "encrypt" or "decrypt": ')
 
@@ -26,36 +27,36 @@ def main():
 	if mode_1 == 'encrypt':
 		# Implement the first algorithm
 		first_cipher = portaCipher(msg, key_1, mode_1)
-		print ('Value of first_cipher is: %s' % first_cipher)
+		#print ('Value of first_cipher is: %s' % first_cipher)
 
 		# Implement the second algorithm
 		second_cipher = transpositionCipher(first_cipher, key_2, mode_2)
-		print ('Value of second_cipher is: %s' % second_cipher)
+		#print ('Value of second_cipher is: %s' % second_cipher)
 
 		# Implement the third algorithm
 		third_cipher = vigenereCipher(second_cipher, key_2, mode_1)
-		print ('Value of third_cipher is: %s' % third_cipher)
+		#print ('Value of third_cipher is: %s' % third_cipher)
 
 		# Write the encrypted data back to the file
-		with open('test.txt', 'w') as file:
+		with open(working_file, 'w') as file:
 			file.write(third_cipher)
 
 
 	elif mode_1 == 'decrypt':
 		# Implement the first algorithm
 		first_cipher = vigenereCipher(msg, key_2, mode_1)
-		print ('Value of first_cipher is: %s' % first_cipher)
+		#print ('Value of first_cipher is: %s' % first_cipher)
 
 		# Implement the second algorithm
 		second_cipher = transpositionCipher(first_cipher, key_2, mode_2)
-		print ('Value of second_cipher is: %s' % second_cipher)
+		#print ('Value of second_cipher is: %s' % second_cipher)
 
 		# Implement the third algorithm
 		third_cipher = portaCipher(second_cipher, key_1, mode_1)
-		print ('Value of third_cipher is: %s' % third_cipher)
+		#print ('Value of third_cipher is: %s' % third_cipher)
 
 		# Write the encrypted data back to the file
-		with open('test.txt', 'w') as file:
+		with open(working_file, 'w') as file:
 			file.write(third_cipher)
 
 
