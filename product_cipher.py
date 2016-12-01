e1af36844e1f8f1df497691baf1080e85eec59c0
Mark Bellingham <14032098@stu.mmu.ac.uk>
2016-12-01 13:53:01 +0000

    encryption/decryption repeated n times - n is length of key

--- product_cipher.py
+++ product_cipher.py
@@ -15,19 +15,20 @@ def encrypt(key_1, mode_1, working_file):
     key_2 = reverse(key_1)
     mode_2 = switchMode(mode_1)
 
-    # Implement the ciphers
-    first_cipher = beaufortCipher(msg, key_1, mode_1)
+    for i in range(len(key_1)):
+        # Implement the ciphers
+        first_cipher = beaufortCipher(msg, key_1, mode_1)
 
-    second_cipher = transpositionCipher(first_cipher, key_2, mode_2)
+        second_cipher = transpositionCipher(first_cipher, key_2, mode_2)
 
-    third_cipher = vigenereCipher(second_cipher, key_2, mode_1)
+        third_cipher = vigenereCipher(second_cipher, key_2, mode_1)
 
-    # Write the encrypted data back to the file
-    with open(working_file, 'w') as file:
-        file.write(third_cipher)
+        # Write the encrypted data back to the file
+        with open(working_file, 'w') as file:
+            file.write(third_cipher)
 
-    # Tell the user about each file that has been encrypted
-    print('File %s has been encrypted' % working_file)
+        # Tell the user about each file that has been encrypted
+        print('File '+ working_file + ' has been encrypted ' + str(i) + ' times' )
 
 
 def decrypt(key_1, mode_1, working_file):
@@ -39,16 +40,17 @@ def decrypt(key_1, mode_1, working_file):
     key_2 = reverse(key_1)
     mode_2 = switchMode(mode_1)
 
-    # Implement the ciphers
-    first_cipher = vigenereCipher(msg, key_2, mode_1)
+    for i in range(len(key_1)):
+        # Implement the ciphers
+        first_cipher = vigenereCipher(msg, key_2, mode_1)
 
-    second_cipher = transpositionCipher(first_cipher, key_2, mode_2)
+        second_cipher = transpositionCipher(first_cipher, key_2, mode_2)
 
-    third_cipher = beaufortCipher(second_cipher, key_1, mode_1)
+        third_cipher = beaufortCipher(second_cipher, key_1, mode_1)
 
-    # Write the encrypted data back to the file
-    with open(working_file, 'w') as file:
-        file.write(third_cipher)
+        # Write the encrypted data back to the file
+        with open(working_file, 'w') as file:
+            file.write(third_cipher)
 
-    # Tell the user about each file that has been decrypted
-    print('File %s has been decrypted' % working_file)
+        # Tell the user about each file that has been decrypted
+        print('File ' + working_file + ' has been decrypted ' + str(i) + ' times')
